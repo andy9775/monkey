f06a2d69ce4ff60698c14bdad4b8e17fa4cb8542
Andy Chrzaszcz <andy9775@gmail.com>
2020-04-23 18:33:17 -0400

    finish parser

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 
 	"github.com/andy9775/interpreter/lexer"
+	"github.com/andy9775/interpreter/parser"
 	"github.com/andy9775/interpreter/token"
 )
 
@@ -25,10 +26,26 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line) // new lexer
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 
 		// parse the tokens from the provided text
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Printf("%+v\n", tok)
 		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
